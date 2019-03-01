605397175e57564c19f9399fa0c06b4c59ad40d7
Djornell12 <Djornell12@gmail.com>
2019-03-01 12:21:56 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'kaas'
+    }
+});
+
+
+var tekst2 = new Vue({
+    el: '#tekst2',
+    data: {
+        tekst: 'dit is een teksts via vue.js'
+    }
+});
